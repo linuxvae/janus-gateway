1323b587362053f8f2e1f496923445b272e7eee0
linuxvae <linuxvae@qq.com>
2019-01-09 17:36:34 +0800

    make sucess

--- plugins/janus_srtc_relay_message.c
+++ plugins/janus_srtc_relay_message.c
@@ -21,7 +21,7 @@ typedef struct {
 	void 	*callee_info;
 	void 	*caller_info;
 	janus_refcount ref;
-}janus_srtc_session_t;
+}srtc_relay_message_session_t;
 
 
 int janus_srtc_relay_create_session(janus_plugin_session *handle, int *error){
@@ -33,14 +33,14 @@ int janus_srtc_relay_destroy_session(janus_plugin_session *handle, int *error){
 	return srtc_destroy_session(handle, error);
 }
 struct janus_plugin_result *
-	janus_srtc_relay_handle_message(janus_plugin_session *handle, char *transaction, json_t *message, json_t *jsep)
+	janus_srtc_relay_handle_message(janus_plugin_session *handle, char *transaction, json_t *root, json_t *jsep)
 {
 
 	json_t *message = json_object_get(root, "srtc");
 	const gchar *message_text = json_string_value(message);
 
-	json_object_t *relay = json_object_get(root, "relay");
-	
+	json_t *relay = json_object_get(root, "relay");
+
 	if(!strcasecmp(message_text, "call")){
 		if(relay){
 			if(signal_server){//信令服务器处理relay的call 直接通过handle中的session sendmessage 发给B
@@ -50,9 +50,9 @@ struct janus_plugin_result *
 			}
 
 		}else if(signal_server){//创建session and创建websocket  ，查找数据库找到callee IP+port进行relay，callback 发送给handle中的session
-			
+
 		}
-		
+
 
 	}else if(!strcasecmp(message_text, "accept")){
 		if(relay){
@@ -66,7 +66,7 @@ struct janus_plugin_result *
 
 		}
 	}else if(!strcasecmp(message_text, "hangup")){//删除各种连接
-		if(relay){			
+		if(relay){
 			if(signal_server){//找到callee 发送
 
 			}else{//不做什么，由videocall模块去处理accept，他会把acctpt发送给session caller
